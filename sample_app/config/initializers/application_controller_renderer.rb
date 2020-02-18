d433c6015ea0f217aaa6bd46a2a4971e1b90fe71
mioyoshino <mio.y@infratop.jp>
2020-02-18 21:23:25 +0000

    [Finish] 3-8

--- sample_app/config/initializers/application_controller_renderer.rb
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '067870cb90b1fe827d04140debb1d7057bf394cbc130ad3ca4b6522c6db5c0f83f20c398e14d52b7784990a074113ea8e6763a1be73105fdf0011f7ca33a8543'
